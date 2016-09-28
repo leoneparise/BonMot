6074425773a747388bf8ad2cd16733c5385d3c25
Leone Parise <leone.parise@gmail.com>
2016-09-28 15:53:06 -0300

    Carthage support

---
+++ BonMot/BonMot.h
@@ -0,0 +1,23 @@
+//
+//  BonMot.h
+//  BonMot
+//
+//  Created by Leone Parise Vieira da Silva on 28/09/16.
+//  Copyright © 2016 com.raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BonMot.
+FOUNDATION_EXPORT double BonMotVersionNumber;
+
+//! Project version string for BonMot.
+FOUNDATION_EXPORT const unsigned char BonMotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BonMot/PublicHeader.h>
+
+#import "BONText.h"
+#import "BONChain.h"
+#import "BONSpecial.h"
+#import "UIImage+BonMotUtilities.h"
+#import "BONTag.h"
---
+++ BonMot_UIKit/BonMot_UIKit.h
@@ -0,0 +1,23 @@
+//
+//  BonMot_UIKit.h
+//  BonMot_UIKit
+//
+//  Created by Leone Parise Vieira da Silva on 28/09/16.
+//  Copyright © 2016 com.raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BonMot_UIKit.
+FOUNDATION_EXPORT double BonMot_UIKitVersionNumber;
+
+//! Project version string for BonMot_UIKit.
+FOUNDATION_EXPORT const unsigned char BonMot_UIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BonMot_UIKit/PublicHeader.h>
+
+
+#import "BONTextAlignmentConstraint.h"
+#import "UILabel+BonMotUtilities.h"
+#import "UITextField+BonMotUtilities.h"
+#import "UITextView+BonMotUtilities.h"
